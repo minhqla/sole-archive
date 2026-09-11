cb7ef5039960e9888a59a9b63fd580273d4ff84e
minhqla <minhqla92@gmail.com>
2026-09-11 15:25:01 +0100

    Add signup views

--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+# SIGNUP - create a new user account, then log them straight in
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect('collection_list')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form': form})

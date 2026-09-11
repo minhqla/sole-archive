389cc7abdb0840f27f17bd14ff01e8f75c325d45
minhqla <minhqla92@gmail.com>
2026-09-11 15:51:52 +0100

    Add collection create views

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import CollectionItem
+from .forms import CollectionItemForm
 
 # Create your views here.
 # SIGNUP - create a new user account, then log them straight in
@@ -26,3 +28,19 @@ def collection_list(request):
 def collection_detail(request, pk):
     item = get_object_or_404(CollectionItem, pk=pk, user=request.user)
     return render(request, 'collection/collection_detail.html', {'item': item})
+
+
+# CREATE - add a new item to the collection
+@login_required
+def collection_create(request):
+    if request.method == 'POST':
+        form = CollectionItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_item = form.save(commit=False)  # don't save to db yet
+            new_item.user = request.user         # attach the logged in user
+            new_item.save()
+            return redirect('collection_list')
+    else:
+        form = CollectionItemForm()
+
+    return render(request, 'collection/collection_form.html', {'form': form})

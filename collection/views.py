6c03d98a3a7f312c84f5052f1df3a509ac9db404
minhqla <minhqla92@gmail.com>
2026-09-22 11:05:58 +0100

    Correct more spelling

--- collection/views.py
+++ collection/views.py
@@ -37,8 +37,8 @@ def collection_list(request):
         items = items.filter(status='owned')
     elif selected_status == 'wishlist':
         items = items.filter(status='wishlist')
-    elif selected_status == 'favorites':
-        items = items.filter(is_favorite=True)
+    elif selected_status == 'favourites':
+        items = items.filter(is_favourite=True)
     # if selected_status is anything else (or missing), show everything
 
     return render(request, 'collection/collection_list.html', {

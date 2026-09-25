908fa03bd0c18838c0b55ef7f4503f06d230e463
minhqla <minhqla92@gmail.com>
2026-09-25 21:27:12 +0100

    Fix STATICFILES_STORAGE for Heroku

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
